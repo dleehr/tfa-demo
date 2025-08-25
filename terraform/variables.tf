fcb4569da8da0e6ff4872950648662948fc111d4
Dan Leehr <dleehr@redhat.com>
2025-08-24 20:39:54 -0400

    Temporarily use nonsensitive variable for eventstreams username/password
    - Sensitive variable or data from vault is marked as sensitive, which is not yet implemented

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,3 +26,11 @@ variable login_approle_secret_id {
 variable vault_namespace {
   type = string
 }
+
+variable aap_event_stream_username {
+  type = string
+}
+
+variable aap_event_stream_password {
+  type = string
+}
